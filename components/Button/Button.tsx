b567025c5a1365eccea470fe989b34486308af3d
Andrii <andreiverkienko1095@gmail.com>
2022-11-11 21:48:30 +0100

    add detailed html props for buttons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -2,13 +2,14 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
-export const Button = ({ appearence, children }: ButtonProps): JSX.Element => {
+export const Button = ({ appearence, children, className, ...props }: ButtonProps): JSX.Element => {
 	return (
 		<button
-			className={cn(styles.button, {
+			className={cn(styles.button, className, {
 				[styles.primary]: appearence == 'primary',
 				[styles.ghost]: appearence == 'ghost',
 			})}
+			{...props}
 		>
 			{children}
 		</button>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home(): JSX.Element {
 	return (
 		<>
 			<Htag tag="h1">Text</Htag>
-			<Button appearence="primary">Кнопка</Button>
+			<Button appearence="primary" className="dsdsd">Кнопка</Button>
 			<Button appearence="ghost">Кнопка</Button>
 		</>
 	);

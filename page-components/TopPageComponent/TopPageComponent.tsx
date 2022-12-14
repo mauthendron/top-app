c7538106fcc80410f871a0282fa3f19bc3912043
Andrii <andreiverkienko1095@gmail.com>
2022-12-14 18:12:17 +0100

    add Sort component

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,7 +1,8 @@
-import {Htag, P, Card, Tag, HhData, Advantages } from '../../components';
+import {Htag, Tag, HhData, Advantages, Sort } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
 import styles from './TopPageComponent.module.css';
 import { TopLevelCategory } from '../../interfaces/page.interface';
+import { SortEnum } from '../../components/Sort/Sort.props';
 
 
 export const TopPageComponent = ({ page, products, firstCategory }: TopPageComponentProps): JSX.Element => {
@@ -10,7 +11,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 			<div className={styles.title}>
 				<Htag tag='h1'>{page.title}</Htag>
 				{products && <Tag color='gray' size='m'>{products.length}</Tag>}
-				<span>Сортировка</span>
+				<Sort sort={SortEnum.Rating} setSort={() => {}}/>
 			</div>
 			<div>
 				{products && products.map(p => (<div key={p._id}>{p.title}</div>))}
@@ -26,7 +27,7 @@ export const TopPageComponent = ({ page, products, firstCategory }: TopPageCompo
 					<Advantages advantages={page.advantages}/>
 				</>
 			}
-			{page.seoText && <P>{page.seoText}</P>}
+			{page.seoText && <div className={styles.seo} dangerouslySetInnerHTML={{__html: page.seoText}}/>}
 			<Htag tag='h2'>Получаемые навыки</Htag>
 			{page.tags.map(t => <Tag key={t} color='primary'>{t}</Tag>)}
 		</div>

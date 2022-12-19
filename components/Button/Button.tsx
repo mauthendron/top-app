7cac64991b9b6fb3ff33c353c2eb9fa320519b47
Andrii <andreiverkienko1095@gmail.com>
2022-12-19 22:19:16 +0100

    add Product resppnsive

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -3,12 +3,12 @@ import { ButtonProps } from "./Button.props";
 import ArrowIcon from "./arrow.svg";
 import cn from "classnames";
 
-export const Button = ({ appearence, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
+export const Button = ({ appearance, arrow = 'none', children, className, ...props }: ButtonProps): JSX.Element => {
 	return (
 		<button
 			className={cn(styles.button, className, {
-				[styles.primary]: appearence == 'primary',
-				[styles.ghost]: appearence == 'ghost',
+				[styles.primary]: appearance == 'primary',
+				[styles.ghost]: appearance == 'ghost',
 			})}
 			{...props}
 		>
--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,14 +1,15 @@
-import { ProductProps } from "./Product.props";
+import { ProductProps } from './Product.props';
 import styles from './Product.module.css';
-import { Card } from "../Card/Card";
-import { Rating } from "../Rating/Rating";
-import { Tag } from "../Tag/Tag";
-import { Button } from "../Button/Button";
-import { declOfNum, priceRu } from "../../helpers/helpers";
-import { Divider } from "../Divider/Divider";
+import cn from 'classnames';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
+import { Button } from '../Button/Button';
+import { declOfNum, priceRu } from '../../helpers/helpers';
+import { Divider } from '../Divider/Divider';
 import Image from 'next/image';
 
-export const Product = ({product, className}: ProductProps): JSX.Element => {
+export const Product = ({ product, className, ...props }: ProductProps): JSX.Element => {
 	return (
 		<Card className={styles.product}>
 			<div className={styles.logo}>
@@ -22,19 +23,19 @@ export const Product = ({product, className}: ProductProps): JSX.Element => {
 			<div className={styles.title}>{product.title}</div>
 			<div className={styles.price}>
 				{priceRu(product.price)}
-				{product.oldPrice && <Tag className={styles.oldPrice} color='green'>{priceRu(product.price - product.oldPrice)}</Tag>}
+				{product.oldPrice && <Tag className={styles.oldPrice} color="green">{priceRu(product.price - product.oldPrice)}</Tag>}
 			</div>
 			<div className={styles.credit}>
 				{priceRu(product.credit)}/<span className={styles.month}>мес</span>
 			</div>
-			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating}/></div>
+			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
 			<div className={styles.tags}>{product.categories.map(c => <Tag key={c} className={styles.category} color='ghost'>{c}</Tag>)}</div>
 			<div className={styles.priceTitle}>цена</div>
 			<div className={styles.creditTitle}>кредит</div>
 			<div className={styles.rateTitle}>{product.reviewCount} {declOfNum(product.reviewCount, ['отзыв', 'отзыва', 'отзывов'])}</div>
-			<Divider className={styles.hr}/>
+			<Divider className={styles.hr} />
 			<div className={styles.description}>{product.description}</div>
-			<div className={styles.features}>
+			<div className={styles.feature}>
 				{product.characteristics.map(c => (
 					<div className={styles.characteristics} key={c.name}>
 						<span className={styles.characteristicsName}>{c.name}</span>
@@ -48,15 +49,15 @@ export const Product = ({product, className}: ProductProps): JSX.Element => {
 					<div className={styles.advTitle}>Преимущества</div>
 					<div>{product.advantages}</div>
 				</div>}
-					{product.disadvantages && <div className={styles.disadvantages}>
-						<div className={styles.advTitle}>Недостатки</div>
-						<div>{product.disadvantages}</div>
-					</div>}
+				{product.disadvantages && <div className={styles.disadvantages}>
+					<div className={styles.advTitle}>Недостатки</div>
+					<div>{product.disadvantages}</div>
+				</div>}
 			</div>
-			<Divider className={styles.hr}/>
+			<Divider className={cn(styles.hr, styles.hr2)} />
 			<div className={styles.actions}>
-				<Button appearence='primary'>Узнать подробнее</Button>
-				<Button appearence='ghost'arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
+				<Button appearance='primary'>Узнать подробнее</Button>
+				<Button appearance='ghost' arrow={'right'} className={styles.reviewButton}>Читать отзывы</Button>
 			</div>
 		</Card>
 	);

6266dde2ce8f09a66c9ee0b9410e2a4fc8f22ff2
Andrii <andreiverkienko1095@gmail.com>
2022-12-25 21:06:31 +0100

    add forwardRef to components

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,22 +1,23 @@
-import { useEffect, useState, KeyboardEvent } from "react";
+import { useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef } from "react";
 import { RatingProps } from "./Rating.props";
 import styles from "./Rating.module.css";
 import cn from "classnames";
 import StarIcon from "./star.svg";
 
-
-export const Rating = ({
+// eslint-disable-next-line react/display-name
+export const Rating = forwardRef(({
 		isEditable = false,
 		rating,
 		setRating,
 		...props
-	}: RatingProps): JSX.Element => {
+	}: RatingProps, ref: ForwardedRef<HTMLDivElement>): JSX.Element => {
 		const [ratingArray, setRatingArray] = useState<JSX.Element[]>(
 			new Array(5).fill(<></>)
 		);
 
 	useEffect(() => {
 		constructRating(rating);
+		// eslint-disable-next-line
 	}, [rating]);
 
 	const constructRating = (currentRating: number) => {
@@ -66,10 +67,10 @@ export const Rating = ({
 	};
 
 	return (
-		<div {...props}>
+		<div {...props} ref={ref}>
 			{ratingArray.map((r, i) => (
 				<span key={i}>{r}</span>
 			))}
 		</div>
 	);
-};
+});
--- components/TextArea/TextArea.tsx
+++ components/TextArea/TextArea.tsx
@@ -1,9 +1,11 @@
 import { TextareaProps } from "./Textarea.props";
 import styles from './Textarea.module.css';
 import cn from 'classnames';
+import { ForwardedRef, forwardRef } from "react";
 
-export const Textarea = ({className, ...props}: TextareaProps): JSX.Element => {
+// eslint-disable-next-line react/display-name
+export const Textarea = forwardRef(({className, ...props}: TextareaProps, ref: ForwardedRef<HTMLTextAreaElement>): JSX.Element => {
 	return (
-		<textarea className={cn(className, styles.textarea)} {...props}/>
+		<textarea className={cn(className, styles.textarea)} ref={ref} {...props}/>
 	);
-};
+});
